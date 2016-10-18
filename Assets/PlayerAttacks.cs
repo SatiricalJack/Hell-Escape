eaaefff949978006c872d471b9f6c9064dda2ae9
fezzthemonk <Spenser.Crutchfield@gmail.com>
2016-10-18 15:23:09 -0400

    Hell-Escape Game
    This is the game in sofar. {Fuck mike get money}

---
+++ Assets/PlayerAttacks.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttacks : MonoBehaviour {
+	//Checks for what you have baught
+	public GameObject shop;
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
